659b1ed2d92e77f2bd316d99069e9c1e13bb9970
Besnik Beka <besnikbeka2001@gmail.com>
2025-02-24 16:04:58 +0100

    add-to-list component update

--- src/app/pages/add-to-list/add-to-list.component.ts
+++ src/app/pages/add-to-list/add-to-list.component.ts
@@ -1,37 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trigger, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-add-to-list',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './add-to-list.component.html',
-  styleUrl: './add-to-list.component.scss'
+  styleUrl: './add-to-list.component.scss',
+  animations: [
+    trigger('listAnimation', [
+      transition(':enter', [
+        style({ height: '0px', opacity: 0 }),
+        animate('0.3s ease-in-out', style({ height: '*', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-in-out', style({ height: '0px', opacity: 0 }))
+      ])
+    ])
+  ]
 })
-export class AddToListComponent implements OnInit{
-  myForm!:FormGroup;
-  listArray:string[]= [];
+export class AddToListComponent implements OnInit {
+  myForm!: FormGroup;
+  listArray: string[] = [];
 
-  constructor(private formBuilder: FormBuilder){}
+  @ViewChildren('listItems') listItems!: QueryList<ElementRef>;
 
-  ngOnInit(){
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
     this.buildForm();
   }
 
-  buildForm(){
+  buildForm() {
     this.myForm = this.formBuilder.group({
-      input:['',Validators.required]
+      input: ['', Validators.required]
     })
   }
 
-  handleAdd(){
-    if(this.myForm.valid){
+  toggleDecoration(index: number) {
+    const element = this.listItems.toArray()[index].nativeElement;
+    element.style.textDecoration = element.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+  }
+
+  handleAdd() {
+    if (this.myForm.valid) {
       this.listArray.push(this.myForm.get('input')?.value);
       this.myForm.reset();
       console.log(this.listArray);
     }
   }
-  handleDelete(index:number){
+  handleDelete(index: number) {
     this.listArray.splice(index, 1);
   }
 }
