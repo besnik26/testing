a30148d3b3e672d3fe5c3cb06a5d3de43a5dc4f0
Besnik Beka <besnikbeka2001@gmail.com>
2025-02-25 17:16:31 +0100

    Multi-step form update

--- src/app/pages/multi-step-form/multi-step-form.component.ts
+++ src/app/pages/multi-step-form/multi-step-form.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-multi-step-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './multi-step-form.component.html',
   styleUrl: './multi-step-form.component.scss'
 })
-export class MultiStepFormComponent {
+export class MultiStepFormComponent implements OnInit {
   step: number = 1
+  myForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.myForm = this.formBuilder.group({
+      personalInfo: this.formBuilder.group({
+        name: ['', Validators.required],
+        surname: ['', Validators.required],
+        email: ['', Validators.required],
+        phone: ['', Validators.required]
+      }),
+      addressInfo: this.formBuilder.group({
+        country: ['', Validators.required],
+        city: ['', Validators.required],
+        street: ['', Validators.required]
+      })
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
+  onBack() {
+    this.step--;
+  }
+
+  onNext() {
+    if (this.step === 1) {
+      if (this.myForm.get('personalInfo')?.valid) {
+        this.step++;
+      }
+      else {
+        this.markFormGroupTouched(this.myForm.get('personalInfo') as FormGroup);
+      }
+    } else if (this.step === 2) {
+      if (this.myForm.get('addressInfo')?.valid) {
+        this.step++;
+      }
+      else {
+        this.markFormGroupTouched(this.myForm.get('addressInfo') as FormGroup);
+      }
+    } else if (this.step === 3) {
+      if (this.myForm.valid) {
+        this.onSubmit();
+      }
+    }
+  }
+
+  markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+    })
+  }
+
+
+
 }
